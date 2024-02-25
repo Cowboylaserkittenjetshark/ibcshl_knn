a3649ff6378e3df7bca829667ea3139fbae5f9af
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-02-24 23:55:59 -0500

    Initial model

---
+++ model.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import pathlib
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OrdinalEncoder, OneHotEncoder
+
+
+ROOT = pathlib.Path(__file__).parent.resolve().joinpath("data")
+
+# reading in data
+data = pd.read_csv(ROOT.joinpath("data.csv"))
+feature_names = [
+"Age",
+"BusinessTravel",
+"DailyRate",
+"Department",
+"DistanceFromHome",
+"Education",
+"EducationField",
+"EmployeeCount",
+"EnvironmentSatisfaction",
+"Gender",
+"HourlyRate",
+"JobInvolvement",
+"JobLevel",
+"JobRole",
+"JobSatisfaction",
+"MaritalStatus",
+"MonthlyIncome",
+"MonthlyRate",
+"NumCompaniesWorked",
+"Over18",
+"OverTime",
+"PercentSalaryHike",
+"PerformanceRating",
+"RelationshipSatisfaction",
+"StandardHours",
+"StockOptionLevel",
+"TotalWorkingYears",
+"TrainingTimesLastYear",
+"WorkLifeBalance",
+"YearsAtCompany",
+"YearsInCurrentRole",
+"YearsSinceLastPromotion",
+"YearsWithCurrManager"
+]
+
+X = data[feature_names]
+y = data["Attrition"]
+le = LabelEncoder()
+y = le.fit_transform(y)
+target_values = le.classes_
+
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
+
+# Column transformer
+column_transformer = make_column_transformer(
+    (OneHotEncoder(handle_unknown='ignore'),
+        [
+            'BusinessTravel',
+            'Department',
+            'EducationField',
+            'Gender',
+            'JobRole',
+            'MaritalStatus', # Ordinal?
+            'Over18', # Ordinal?
+            'OverTime', # Ordinal?
+        ]
+    ),
+    (OrdinalEncoder(),
+        []
+    ),
+    remainder='passthrough'
+)
+
+# Pipeline applying scaler and knn
+clf = Pipeline(
+    steps=[("column_transformer", column_transformer), ("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=11))]
+)
+
+clf.fit(X_train, y_train)
+print("model score: %.3f" % clf.score(X_test, y_test))
