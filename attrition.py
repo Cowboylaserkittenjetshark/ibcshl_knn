8bd2fc463f1ee16ce87a0813394673e51395a470
lagisreal89 <56201840+lagisreal89@users.noreply.github.com>
2024-03-05 20:06:50 +0000

    trying out different hyperparams

--- attrition.py
+++ attrition.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import pathlib
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
 from sklearn.compose import make_column_transformer
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OrdinalEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OrdinalEncoder, OneHotEncoder, MinMaxScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
@@ -12,6 +15,8 @@ ROOT = pathlib.Path(__file__).parent.resolve().joinpath("data")
 
 # reading in data
 data = pd.read_csv(ROOT.joinpath("data.csv"))
+data['OverTime'] = data['OverTime'].map({'Yes': 1, 'No': 0})
+data['Gender'] = data['Gender'].map({'Male': 1, 'Female': 0})
 feature_names = [
 "Age",
 #"BusinessTravel",
@@ -20,7 +25,7 @@ feature_names = [
 "Education",
 #"EducationField",
 "EnvironmentSatisfaction",
-#"Gender",
+"Gender",
 "HourlyRate",
 "JobInvolvement",
 "JobLevel",
@@ -30,7 +35,7 @@ feature_names = [
 "MonthlyIncome",
 "NumCompaniesWorked",
 #"Over18",
-#"OverTime",
+"OverTime",
 "PercentSalaryHike",
 "PerformanceRating",
 "RelationshipSatisfaction",
@@ -48,8 +53,6 @@ y = data["Attrition"]
 le = LabelEncoder()
 y = le.fit_transform(y)
 target_values = le.classes_
-print(data.dtypes)
-#print(data.describe)
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
 
@@ -59,11 +62,11 @@ column_transformer = make_column_transformer(
         [
             #'Department',
             #'EducationField',
-            #'Gender',
+            'Gender',
             #'JobRole',
             #'MaritalStatus',
             #'Over18',
-            #'OverTime',
+            'OverTime',
         ]
     ),
     (OrdinalEncoder(),
@@ -76,12 +79,12 @@ column_transformer = make_column_transformer(
 
 # Pipeline applying scaler and knn
 clf = Pipeline(
-    steps=[("column_transformer", column_transformer), ("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=15, metric="euclidean"))]
+    steps=[("column_transformer", column_transformer), ("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=15, metric="manhattan"))]
 )
 
 clf.fit(X_train, y_train)
 print("model score: %.3f" % clf.score(X_test, y_test))
-train_score = {}
+'''train_score = {}
 test_score = {}
 n_neighbors = np.arange(2, 30, 1)
 for neighbor in n_neighbors:
@@ -99,3 +102,19 @@ plt.xlim(0, 33)
 plt.ylim(0.60, 0.90)
 plt.grid()
 plt.show()
+for key, value in test_score.items():
+    if value==max(test_score.values()):
+        print(key)'''
+knn_pipe = Pipeline([('mms', MinMaxScaler()),
+                     ('knn', KNeighborsClassifier())])
+params = [{'knn__n_neighbors': [9, 11, 13, 15],
+         'knn__weights': ['uniform', 'distance'],
+         'knn__leaf_size': [15, 20]}]
+gs_knn = GridSearchCV(knn_pipe,
+                      param_grid=params,
+                      scoring='accuracy',
+                      cv=5)
+gs_knn.fit(X_train, y_train)
+gs_knn.best_params_
+# find best model score
+print(gs_knn.score(X_train, y_train))
