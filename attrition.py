213178d86dc5f9d00014ba2075dd270dc0535eba
lagisreal89 <56201840+lagisreal89@users.noreply.github.com>
2024-03-05 18:51:37 +0000

    trying to hyperparameter stuff

--- attrition.py
+++ attrition.py
@@ -5,31 +5,32 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OrdinalEncoder, OneHotEncoder
-
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 ROOT = pathlib.Path(__file__).parent.resolve().joinpath("data")
 
 # reading in data
 data = pd.read_csv(ROOT.joinpath("data.csv"))
 feature_names = [
 "Age",
-"BusinessTravel",
-"Department",
+#"BusinessTravel",
+#"Department",
 "DistanceFromHome",
 "Education",
-"EducationField",
+#"EducationField",
 "EnvironmentSatisfaction",
-"Gender",
+#"Gender",
 "HourlyRate",
 "JobInvolvement",
 "JobLevel",
-"JobRole",
+#"JobRole",
 "JobSatisfaction",
-"MaritalStatus",
+#"MaritalStatus",
 "MonthlyIncome",
 "NumCompaniesWorked",
-"Over18",
-"OverTime",
+#"Over18",
+#"OverTime",
 "PercentSalaryHike",
 "PerformanceRating",
 "RelationshipSatisfaction",
@@ -47,8 +48,8 @@ y = data["Attrition"]
 le = LabelEncoder()
 y = le.fit_transform(y)
 target_values = le.classes_
-
-
+print(data.dtypes)
+#print(data.describe)
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
 
@@ -56,18 +57,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_sta
 column_transformer = make_column_transformer(
     (OneHotEncoder(handle_unknown='ignore'),
         [
-            'Department',
-            'EducationField',
-            'Gender',
-            'JobRole',
-            'MaritalStatus',
-            'Over18',
-            'OverTime',
+            #'Department',
+            #'EducationField',
+            #'Gender',
+            #'JobRole',
+            #'MaritalStatus',
+            #'Over18',
+            #'OverTime',
         ]
     ),
     (OrdinalEncoder(),
         [
-            'BusinessTravel'
+            #'BusinessTravel'
         ]
     ),
     remainder='passthrough'
@@ -75,8 +76,26 @@ column_transformer = make_column_transformer(
 
 # Pipeline applying scaler and knn
 clf = Pipeline(
-    steps=[("column_transformer", column_transformer), ("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=11, metric="euclidean"))]
+    steps=[("column_transformer", column_transformer), ("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=15, metric="euclidean"))]
 )
 
 clf.fit(X_train, y_train)
 print("model score: %.3f" % clf.score(X_test, y_test))
+train_score = {}
+test_score = {}
+n_neighbors = np.arange(2, 30, 1)
+for neighbor in n_neighbors:
+    knn = KNeighborsClassifier(n_neighbors=neighbor)
+    knn.fit(X_train, y_train)
+    train_score[neighbor]=knn.score(X_train, y_train)
+    test_score[neighbor]=knn.score(X_test, y_test)
+plt.plot(n_neighbors, train_score.values(), label="Train Accuracy")
+plt.plot(n_neighbors, test_score.values(), label="Test Accuracy")
+plt.xlabel("Number Of Neighbors")
+plt.ylabel("Accuracy")
+plt.title("KNN: Varying number of Neighbors")
+plt.legend()
+plt.xlim(0, 33)
+plt.ylim(0.60, 0.90)
+plt.grid()
+plt.show()
